fea1061626e39e6631bdef78497d32c82b356562
lhypoi <904579880@qq.com>
2023-07-21 14:09:06 +0800

    feat: 调整路由层捕获登出时的登录地址

--- src/permission.js
+++ src/permission.js
@@ -4,7 +4,7 @@ import store from './store'
 import storage from 'store'
 import NProgress from 'nprogress' // progress bar
 import '@/components/NProgress/nprogress.less' // progress bar custom style
-import notification from 'ant-design-vue/es/notification'
+// import notification from 'ant-design-vue/es/notification'
 import { setDocumentTitle, domTitle } from '@/utils/domUtil'
 import { ACCESS_TOKEN } from '@/store/mutation-types'
 import { i18nRender } from '@/locales'
@@ -25,7 +25,7 @@ const allowList = [
   'Download',
   'Login'
 ] // no redirect allowList
-const loginRoutePath = '/user/login'
+const loginRoutePath = '/login'
 const defaultRoutePath = '/home'
 
 router.beforeEach((to, from, next) => {
@@ -65,10 +65,10 @@ router.beforeEach((to, from, next) => {
             })
           })
           .catch(() => {
-            notification.error({
-              message: '错误',
-              description: '请求用户信息失败，请重试'
-            })
+            // notification.error({
+            //   message: '错误',
+            //   description: '请求用户信息失败，请重试'
+            // })
             // 失败时，获取用户信息失败时，调用登出，来清空历史保留信息
             store.dispatch('Logout').then(() => {
               next({ path: loginRoutePath, query: { redirect: to.fullPath } })
