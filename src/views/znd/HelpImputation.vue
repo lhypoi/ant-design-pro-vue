e542accbac4d12c41d1a4871eae013e9c321d26b
lhypoi <904579880@qq.com>
2023-08-07 14:33:24 +0800

    feat: 首页和help

--- src/views/znd/HelpImputation.vue
+++ src/views/znd/HelpImputation.vue
@@ -1,22 +1,190 @@
 <template>
   <div class="flex-auto flex flex-col bg-white rounded-3xl p-8">
+    <div v-if="loading" class="w-[750px] mx-auto pt-10">
+      <a-skeleton active :paragraph="{ rows: 10 }" />
+    </div>
+    <div v-show="!loading">
+      <div ref="container"></div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Info',
+  name: 'HelpImputation',
   components: {
   },
   data() {
     return {
+      loading: true,
+      htmlStr: `
+  <div class="stackedit__html">
+<h1 id="help">Help</h1>
+<h2 id="introduce">Introduce</h2>
+<p>1kCIGP Imputation Server is a genotype imputation server utilizing the 1kCIGP reference panel constructed from The 1000 Chinese Domestic Pig Genomes Project (1KCIGP) dataset. The users can upload their unphased genotypes to the server and receive the phased and imputed genotypes in return. The haplotypes phasing and genotypes imputation will be performed by using SHAPEIT4 and Minimac4.</p>
+<p><img src="/znd/Snipaste_2023-08-03_19-00-18.png" alt=""></p>
+<h2 id="reference-panel">Reference Panel</h2>
+<p>Our server offers imputation with the following reference panels:</p>
+
+<table>
+<thead>
+<tr>
+<th align="center">Item</th>
+<th align="center">Content</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td align="center">Description</td>
+<td align="center">This reference panel is constructed basing on the 1KCIGP phase1 dataset.</td>
+</tr>
+<tr>
+<td align="center">Number of individuals</td>
+<td align="center">1,011</td>
+</tr>
+<tr>
+<td align="center">Genome build</td>
+<td align="center">S. scrofa 11.1</td>
+</tr>
+<tr>
+<td align="center">Number of sites</td>
+<td align="center">4,583,958</td>
+</tr>
+<tr>
+<td align="center">Chromosomes</td>
+<td align="center">chr1-18,X</td>
+</tr>
+</tbody>
+</table><h2 id="getting-started">Getting started</h2>
+<h3 id="sign-up">Sign up</h3>
+<p>Before using ChinaMAP Imputation Server, you need to have a account and login. You may click here to <a href="http://120.25.231.168:8080/register">register</a> for your account. The server will inform you by email when the imputation tasks are finished or failed, so you need to make sure your email is available.</p>
+<p><img src="/znd/Snipaste_2023-08-03_19-02-53.png" alt=""></p>
+<h3 id="submit-your-imputation-tasks">Submit your imputation tasks</h3>
+<p>You can submit your tasks at the <a href="http://120.25.231.168:8080/imputation">homepage</a>.</p>
+<p>The following information is required.</p>
+<h4 id="task-name">Task Name</h4>
+<p>By default, the name for the imputation task is automatically generated according to the current time, and it can be modified by you when needed.</p>
+<h4 id="reference-panel-1">Reference Panel</h4>
+<p>Choose which reference panel to use for genotype imputation. We offer the following reference panel:</p>
+<p>[1KCIGP.phase1](#Reference Panel)</p>
+<p>More details about the available reference panels can be found here.</p>
+<h4 id="quality-control">Quality Control</h4>
+<p>Choose whether quality control alignment processing is needed. If you choose ‘yes’, we will perform quality control alignment before phase. If you choose ‘no’, you should upload a dataset that has already undergone quality control alignment.</p>
+<h4 id="phased">Phased</h4>
+<p>Choose whether phase alignment processing is needed. If you choose ‘yes’, we will perform phase alignment before impute. If you choose ‘no’, you should upload a dataset that has already undergone phase alignment.</p>
+<h4 id="input-bcf-files">Input BCF Files</h4>
+<p>You can upload bcf files from your computer via clicking on Uploading File and selecting files on the dialog appears. Multiple files can be selected simultaneously. If a file is incorrectly selected, you can delete it by clicking the “deleted” icon that appears to the right of this file. We also provide testing data, which can be used for imputation by clicking on Using Demo Data. The data will be uploaded after you click on Submit Task.</p>
+<p>⚠️ Please read this tutorial about [data preparation](#data preparation) to ensure your data is in the correct format.</p>
+<p>After confirming our terms of service, the imputation task can be started immediately by clicking on Submit Task. You will be informed by email as soon as the job is finished or failed.</p>
+<h3 id="view-your-imputation-tasks">View your imputation tasks</h3>
+<p>Your task list can be shown by clicking on <a href="http://120.25.231.168:8080/profile">My Profile</a>. You can also customize the time range to view the tasks submitted in a specified period.</p>
+<p><img src="/znd/Snipaste_2023-08-03_20-07-26.png" alt=""></p>
+<h3 id="download-the-imputation-results">Download the imputation results</h3>
+<p>When receive an email notifying that the task is finished, you can click the link in the email to download the results.</p>
+<p>⚠️ All data will be preserved on our server only for 3 days.</p>
+<h2 id="data-preparation">data preparation</h2>
+<h3 id="requirements-of-input-files">Requirements of input files</h3>
+<p>Please ensure that your input files meet the following requirements:</p>
+<ul>
+<li>
+<p>The input file is bcf format.</p>
+</li>
+<li>
+<p>The bcf file is sorted by genomic coordinates.</p>
+</li>
+<li>
+<p>S.scrofa 11.1 coordinates are required.</p>
+</li>
+<li>
+<p>The chromosomes should be encoded with prefix ‘chr’ (e.g. chr2).</p>
+</li>
+<li>
+<p>Create separate bcf file for each chromosome.</p>
+</li>
+<li>
+<p>The index of bcf file is unnecessary.</p>
+</li>
+<li>
+<p>Only one file can be uploaded for one chromosome in the same task.</p>
+</li>
+<li>
+<p>Files uploaded at the same time shouldn’t have the same filename. And the filenames shouldn’t contain any special characters and non-ASCII characters.</p>
+</li>
+<li>
+<p>The size of a bcf file shouldn’t exceed 100 MiB.</p>
+</li>
+<li>
+<p>The percentage of sites that are not existed in the 1KCIGP reference panel shouldn’t exceed 50%. Too many such sites may cause QC failure. The list of 1KCIGP reference panel sites can be download here.</p>
+</li>
+</ul>
+<h2 id="data-security-and-privacy">Data Security and Privacy</h2>
+<ul>
+<li>
+<p>All steps (data transfer, data analysis, data storage) are executed on a secure server, hosted on Tencent Cloud, located in Shanghai, China.</p>
+</li>
+<li>
+<p>All nodes for imputation compute can only be accessed on a local area network and only administrators can log in.</p>
+</li>
+<li>
+<p>Users can only view their own tasks and data.</p>
+</li>
+<li>
+<p>All data is preserved on our server only for 3 days. After the imputation is finished or failed, all results will be deleted in 3 days.</p>
+</li>
+<li>
+<p>We only store the number of samples and markers for analyzing. We will never “look” at your data in anyway.</p>
+</li>
+</ul>
+</div>
+      `
     }
   },
   computed: {
   },
   created() {
+    this.loading = true
   },
   async mounted() {
+    const shadowRoot = this.$refs.container.attachShadow({ mode: 'open' })
+    const link = document.createElement('link')
+    link.rel = 'stylesheet'
+    link.href = 'https://stackedit.io/style.css'
+    shadowRoot.appendChild(link)
+    const div = document.createElement('div')
+    div.style.cssText = `
+      color: rgba(0,0,0,.75);
+      font-size: 16px;
+      font-family: Lato,Helvetica Neue,Helvetica,sans-serif;
+      font-variant-ligatures: common-ligatures;
+      line-height: 1.67;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale
+    `
+    div.innerHTML = this.htmlStr
+    shadowRoot.appendChild(div)
+    const links = shadowRoot.querySelectorAll('a')
+    links.forEach(link => {
+      link.addEventListener('click', (event) => {
+        const targetId = event.target.getAttribute('href')
+        const targetElement = shadowRoot.querySelector(targetId)
+        if (targetElement) {
+          targetElement.scrollIntoView({ behavior: 'smooth' })
+        }
+      })
+    })
+
+    setTimeout(() => {
+      this.loading = false
+      const hash = window.location.hash
+      if (hash) {
+        setTimeout(() => {
+          const targetElement = shadowRoot.querySelector(hash)
+          if (targetElement) {
+            targetElement.scrollIntoView({ behavior: 'smooth' })
+          }
+        }, 300)
+      }
+    }, 1000)
   },
   methods: {
   }
--- src/views/znd/Home.vue
+++ src/views/znd/Home.vue
@@ -99,19 +99,37 @@
       </div>
     </div>
     <div class="flex flex-row justify-between px-20 pt-10">
-      <div class="flex flex-col items-center">
-        <div class="text-2xl font-bold pb-2">Version</div>
-        <div>2020-03.beta</div>
+      <div style="width: 33%;">
+        <div id="main" style="height:500px; width:100%;">
+        </div>
       </div>
-      <div class="flex flex-col items-center">
-        <div class="text-2xl font-bold pb-2">Size of data</div>
-        <div>Cohort: 10,588 samples</div>
-        <div>Sequencing bases: 1,380 Tb</div>
-      </div>
-      <div class="flex flex-col items-center">
-        <div class="text-2xl font-bold pb-2">Number of variants</div>
-        <div>SNP: 136,745,826</div>
-        <div>INDEL: 10,703,115</div>
+      <div class="flex flex-row justify-between px-20 pt-10 items-center">
+        <div class="flex flex-col justify-between" style="width: 65%;">
+          <div class="flex flex-col  pb-6">
+            <div class="text-2xl font-bold pb-2">Version</div>
+            <div>2020-08.PHASE1</div>
+          </div>
+          <div class="flex flex-col  pb-6">
+            <div class="text-2xl font-bold pb-2">Size of data</div>
+            <div>Breed(Population):50</div>
+            <div>Cohort: 1,011 samples</div>
+            <div>Sequencing bases: 66 Tb</div>
+          </div>
+          <div class="flex flex-col  pb-6">
+            <div class="text-2xl font-bold pb-2">Number of variants</div>
+            <div>SNP: 50,590,445</div>
+            <div>INDEL: 12,870,753</div>
+          </div>
+        </div>
+        <div class="flex flex-col items-center">
+          <div class="flex flex-col pb-6">
+            <div class="text-2xl font-bold pb-2">The latest news</div>
+            <ul style="list-style-type: circle;">
+              <li>2023-08 BSP function of 1KCIGP has been public available.</li>
+              <li>2023-08 1KCIGP PHASE1 data has been released.</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -119,13 +137,32 @@
 
 <script>
 import { mapGetters } from 'vuex'
-
 export default {
   name: 'Home',
   components: {
   },
   data() {
     return {
+      echarts: {},
+      myData: [
+        { name: 'Beijing', value: '0' }, { name: 'Tianjin', value: '0' },
+        { name: 'Shanghai', value: '1' }, { name: 'Chongqing', value: '0' },
+        { name: 'Hebei', value: '0' }, { name: 'Henan', value: '1' },
+        { name: 'Yunnan', value: '3' }, { name: 'Liaoning', value: '0' },
+        { name: 'Heilongjiang', value: '1' }, { name: 'Hunan', value: '4' },
+        { name: 'Anhui', value: '3' }, { name: 'Shandong', value: '3' },
+        { name: 'Xinjiang', value: '0' }, { name: 'Jiangsu', value: '5' },
+        { name: 'Zhejiang', value: '3' }, { name: 'Jiangxi', value: '3' },
+        { name: 'Hubei', value: '3' }, { name: 'Guangxi', value: '2' },
+        { name: 'Gansu', value: '0' }, { name: 'Shanxi', value: '1' },
+        { name: 'Inner Mongolia', value: '0' }, { name: 'Shaanxi', value: '2' },
+        { name: 'Jilin', value: '0' }, { name: 'Fujian', value: '2' },
+        { name: 'Guizhou', value: '2' }, { name: 'Guangdong', value: '4' },
+        { name: 'Qinghai', value: '0' }, { name: 'Tibet', value: '0' },
+        { name: 'Sichuan', value: '2' }, { name: 'Ningxia', value: '0' },
+        { name: 'Hainan', value: '2' }, { name: 'Taiwan', value: '0' },
+        { name: 'Hongkong', value: '0' }, { name: 'Macao', value: '0' }
+      ],
       typeList: [
         {
           label: 'Gene symbol',
@@ -269,8 +306,63 @@ export default {
   created() {
   },
   async mounted() {
+    this.echarts = window.echarts
+    this.initChart()
   },
   methods: {
+    initChart() {
+      var optionMap = {
+        backgroundColor: '#FFFFFF',
+        title: {
+          text: 'Samples distribution',
+          subtext: '',
+          x: 'center'
+        },
+        tooltip: {
+          trigger: 'item'
+        },
+        // 左侧小导航图标
+        visualMap: {
+        x: 'left',
+        y: 'middle',
+        min: 0,
+        max: 5,
+        inRange: {
+          color: [
+            '#FFFFFF',
+            '#3D6DB5'
+          ]
+        },
+        text: ['High', 'Low'],
+        calculable: true
+      },
+
+        // 配置属性
+        series: [{
+          name: 'Breed number',
+          type: 'map',
+          mapType: 'china',
+          roam: true,
+          label: {
+            normal: {
+              show: false // 省份名称
+            },
+            emphasis: {
+              show: false
+            }
+          },
+          data: this.myData // 数据
+        }]
+      }
+      // 初始化echarts实例
+      var myChart = this.echarts.init(document.getElementById('main'))
+
+      // 使用制定的配置项和数据显示图表
+      myChart.setOption(optionMap)
+    },
+    randomData () {
+      return Math.round(Math.random() * 500)
+    },
     handleSearch() {
       if (!this.hasLogin) {
         this.$router.push({ name: 'Login' })
