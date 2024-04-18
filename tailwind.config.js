a8053815cb703da3ac41c586b3b7556dbff93cff
lhypoi <904579880@qq.com>
2024-04-18 15:17:07 +0800

    feat: Add font import and update body font-family in global.less

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./public/index.html', './src/**/*.{html,js,ts,jsx,tsx,vue}'],
-  mode: 'jit'
+  mode: 'jit',
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['"Alibaba PuHuiTi 2.0 55 Regular"', ...defaultTheme.fontFamily.sans]
+      },
+      colors: {
+        blue: {
+          400: 'rgba(24, 144, 255, 1)'
+        }
+      }
+    }
+  }
 }
