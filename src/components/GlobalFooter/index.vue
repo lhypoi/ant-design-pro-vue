54bd2375699469be2aa065798d16228da050e404
lhypoi <904579880@qq.com>
2024-09-03 15:59:57 +0800

    feat: update

--- src/components/GlobalFooter/index.vue
+++ src/components/GlobalFooter/index.vue
@@ -3,6 +3,14 @@
     <template v-slot:links>
       <a class="text-center" href="https://beian.miit.gov.cn" target="_blank">备案号：京ICP备2023016911号-2</a>
       <span class="text-center mx-10">@1KCIGP</span>
+      <span class="pl-20">
+        <a href="" class="inline-flex items-center gap-1 cursor-pointer">
+          <img src="@/assets/znd/beian.png" style="width: 16px; height: auto" />
+          <span style="color: #939393">
+            京公网安备 11010802042839号
+          </span>
+        </a>
+      </span>
     </template>
     <!-- <template v-slot:copyright>
       <a href="https://github.com/vueComponent" target="_blank">vueComponent</a>
